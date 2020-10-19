49d6e3735af82c85076e802792419af1fdc0ff94
kiernan16 <kiernan.creations@gmail.com>
2020-10-19 12:35:48 -0400

    added comments

--- bertrand_box.py
+++ bertrand_box.py
@@ -11,17 +11,17 @@ gold_picked = 0
 while x < 10000:
     pick_box = random.randrange(0,3)
     
-    if pick_box == 0:
+    if pick_box == 0: #box_A, automatic win
         gold_picked += 1
         x += 1
         
-    elif pick_box == 1:
+    elif pick_box == 1: #box_B, if gold is picked first, it's a loss. If silver is picked first, not counted per scenario
         pick = random.randrange(0,2)
         
         if box_B[pick] == "G":
             x += 1
             
-    elif pick_box == 2:
+    elif pick_box == 2: #box_C, not counted per scenario
         continue
     
     if x % 1000 == 0 and not x == 0:
